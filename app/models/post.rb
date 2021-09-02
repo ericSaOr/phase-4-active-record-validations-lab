57a6a533343ed8f5c008407b712c638e3bd97137
Eric Sanchez <ericfsod@gmail.com>
2021-09-02 12:25:29 -0400

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :is_title_true_facts
+
+    def is_title_true_facts
+       if title == "True Facts"
+        errors.add( :title, "Length of title must be greater than 12 characters" )
+        
+       end 
+    end
 end
